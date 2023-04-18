08ed6ebcff96ed4554ac5d8bc9291229d3c218e0
Marcos <marcos.ferrero001@gmail.com>
2023-04-17 23:40:29 -0400

    update aim role ro allow post in dynamodb

--- sls-rest-api-main/notes/handler.js
+++ sls-rest-api-main/notes/handler.js
@@ -1,10 +1,30 @@
 'use strict';
+const DynamoDB = require("aws-sdk/clients/dynamodb");
+const documentClient = new DynamoDB.DocumentClient({ region: 'us-east-1' });
 
-module.exports.createNote = async (event) => {
-  return {
-    statusCode: 201,
-    body: JSON.stringify("A new note created!"    ),
-  };
+module.exports.createNote = async (event, context, cb) => {
+  let data = JSON.parse(event.body);
+  try {
+    const params = {
+      TableName: "notes",
+      Item: {
+        notesId: data.id,
+        title: data.title,
+        body: data.body
+      },
+      ConditionExpression: "attribute_not_exists(noteId)"
+    }
+    await documentClient.put(params).promise();
+    cb(null, {
+      statusCode: 201,
+      body: JSON.stringify(data)
+    });
+  } catch (err) {
+    cb(null, {
+      statusCode: 500,
+      body: JSON.stringify(err.message)
+    });
+  }  
 };
 
 module.exports.updateNote = async (event) => {
